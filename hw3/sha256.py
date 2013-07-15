a708671f3bc3a5823ee206e3879c85460664f47b
Varren <varren@gmail.com>
2013-07-15 19:48:12 +0400

    hw3 done

---
+++ hw3/sha256.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from Crypto.Hash import SHA256
+import os;
+
+def calcSHA(filename, bufSize, blockPos):
+
+    with open(filename, 'rb') as f:
+        data = f.read()
+    
+    numofBlocks = len(data)/bufSize - blockPos
+    
+    if len(data)%bufSize:
+        numofBlocks += 1 
+    
+    h = ''
+
+    for i in reversed(xrange(0, numofBlocks)):
+        currBlock = data[i * bufSize : (i+1) * bufSize] + h 
+        h = SHA256.new(currBlock).digest()
+        
+                
+    print ''.join(x.encode('hex') for x in h) #SHA256.new(currBlock).hexdigest()
+
+#03c08f4ee0b576fe319338139c045c89c3e8e9409633bea29442e21425006ea8
+calcSHA("video1.mp4", 1024, 0)
+calcSHA("video2.mp4", 1024, 0)
